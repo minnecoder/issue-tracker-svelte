5a3fce7fd779f4ad054a67ae6c1cabcea260758e
Sean Parkin <sdparkin@protonmail.com>
2025-01-16 23:23:49 -0600

    Created layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.css';
+	import Sidebar from '../components/Sidebar.svelte';
+	import Header from '../components/Header.svelte';
+</script>
+
+<div class="layout">
+	<Header />
+	<Sidebar />
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.layout {
+		display: grid;
+		grid-template-columns: 200px 1fr 1fr;
+		grid-template-rows: 100px 1fr;
+		grid-template-areas:
+			'header header header'
+			'sidebar main main';
+		grid-gap: 0;
+		background: white;
+	}
+	main {
+		flex: 1;
+		padding: 1rem;
+	}
+</style>
